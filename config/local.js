2811777674c4d3ca6018034cdd0924c33a8a907b
antoine <duvillierantoine@gmail.com>
2014-05-04 23:48:31 +0200

    set up postgresql connection

--- config/local.js
+++ config/local.js
@@ -62,6 +62,10 @@ module.exports = {
   // By default, Sails sets its environment using the `NODE_ENV` environment variable.
   // If NODE_ENV is not set, Sails will run in the 'development' environment.
 
-  environment: process.env.NODE_ENV || 'development'
+  environment: process.env.NODE_ENV || 'development',
+
+  models: {
+    connection: 'localDiskDb'
+  }
 
 };
--- config/models.js
+++ config/models.js
@@ -11,6 +11,7 @@ module.exports.models = {
   // Your app's default connection.
   // i.e. the name of one of your app's connections (see `config/connections.js`)
   //
-  // (defaults to localDiskDb)
-  connection: 'localDiskDb'
+  // (defaults to Postgresql)
+  // for local connection, see (`config/local.js`)
+  connection: 'Postgresql'
 };
