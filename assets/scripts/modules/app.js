1ca60d057dfe9a513498b0c5c4e912f0ff01f40b
Duvillier Antoine <duvillierantoine@gmail.com>
2014-05-27 15:43:50 +0200

    set tooltip team social

--- assets/scripts/modules/app.js
+++ assets/scripts/modules/app.js
@@ -1,10 +1,14 @@
 import login from 'registration/login';
 import signup from 'registration/signup';
 
+import team from 'team/index';
+
 (function ($) {
   'use strict';
 
   login();
   signup();
+
+  team();
 
 })(window.jQuery);
---
+++ assets/scripts/modules/team/index.js
@@ -0,0 +1,5 @@
+var index = function () {
+  $('.team .social').tooltip();
+};
+export
+default index;
