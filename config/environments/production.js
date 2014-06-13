10eb362f79b41f394752f735380bfd6bec14f8d7
Duvillier Antoine <duvillierantoine@gmail.com>
2014-06-13 15:25:25 +0200

    upgrade sails-postgresql orm to enable url config property

--- config/environments/production.js
+++ config/environments/production.js
@@ -12,7 +12,8 @@ module.exports = {
       port: process.env.DATABASE_PORT,
       user: process.env.DATABASE_USER,
       password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME
+      database: process.env.DATABASE_NAME,
+      ssl: true
     }
   },
 
