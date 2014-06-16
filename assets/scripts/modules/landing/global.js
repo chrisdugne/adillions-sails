2fbc2d519dbe29c24594a6f64e0563135bb32a35
Duvillier Antoine <duvillierantoine@gmail.com>
2014-06-16 18:13:51 +0200

    set google maps

--- assets/scripts/modules/landing/global.js
+++ assets/scripts/modules/landing/global.js
@@ -2,6 +2,25 @@ var global = function () {
   // enable tooltip
   $('#js-global-players .badge-wrap').tooltip();
 
+  google.maps.event.addDomListener(window, 'load', function(){
+    var map = new google.maps.Map(document.getElementById('js-global-map'), {
+      panControl: false,
+      zoomControl: false,
+      scaleControl: false,
+      scrollwheel: false,
+      draggable: false,
+      overviewMapControl: false,
+      streetViewControl: false,
+      mapTypeControl: false,
+      disableDoubleClickZoom: true,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      mapTypeControlOptions: {
+        mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'simple']
+      }
+    });
+  });
+
+
 };
 export
 default global;
