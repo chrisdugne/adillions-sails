a95c6c1db3314193fc87cf75315e96c168556dab
Duvillier Antoine <duvillierantoine@gmail.com>
2014-07-01 14:28:04 +0200

    refactor databse connection properties

--- config/environments/production.js
+++ config/environments/production.js
@@ -7,12 +7,7 @@ module.exports = {
   connections: {
     Postgresql: {
       adapter: 'sails-postgresql',
-      url: process.env.DATABASE_URL,
-      host: process.env.DATABASE_HOST,
-      port: process.env.DATABASE_PORT,
-      user: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
+      url: process.env.DATABASE_URL
       ssl: true
     }
   },
