01c58bd8154909bd363905465559331e9b9399a7
chrisdugne <chris.dugne@uralys.com>
2014-08-01 23:29:14 +0200

    started menu

---
+++ webapp/lib/views/profile-view.js
@@ -0,0 +1,7 @@
+
+var Marionette = require('Marionette'),
+    template   = require('../../templates/profile.hbs');
+
+var Profile = module.exports = Marionette.ItemView.extend({
+    template:  template
+});
---
+++ webapp/lib/views/tickets-view.js
@@ -0,0 +1,7 @@
+
+var Marionette = require('Marionette'),
+    template   = require('../../templates/tickets.hbs');
+
+var Tickets = module.exports = Marionette.ItemView.extend({
+    template:  template
+});
