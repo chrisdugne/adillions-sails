ef1a984c89ccbc975b875dec7b00e08dcaaa3fd2
Duvillier Antoine <duvillierantoine@gmail.com>
2014-09-04 00:28:26 -0700

    finally, render visible the dev conf as it use same for everyone

---
+++ config/environments/development.js
@@ -0,0 +1,31 @@
+module.exports = {
+
+  log: {
+    level: 'verbose'
+  },
+
+  connections: {
+    Postgresql: {
+      adapter: 'sails-postgresql',
+      url: 'postgres://oubcukdgrmjfrz:Yepe_9jrgK0yIVuyhpan-JrWH5@ec2-54-228-224-127.eu-west-1.compute.amazonaws.com:5432/d4pq9slihvgnns',
+      ssl: true
+    }
+  },
+
+  models: {
+    connection: 'Postgresql'
+  },
+
+  passport: {
+    facebook: {
+      options: {
+        // legacy dev
+        // clientID: '534196239997712',
+        // clientSecret: '46383d827867d50ef5d87b66c81f1a8e'
+        clientID: '1485626205018543',
+        clientSecret: '44cf8a3cb8235821dec1fe5b0257968e'
+      }
+    }
+  }
+
+};
