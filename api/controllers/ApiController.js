ed200ee7311447f06f92e03f4a88bd1c6cafbdbc
Duvillier Antoine <duvillierantoine@gmail.com>
2014-09-13 19:55:00 -0700

    API controller update

--- api/controllers/ApiController.js
+++ api/controllers/ApiController.js
@@ -1,7 +1,43 @@
-var _ = require('lodash');
+var ApiController = module.exports = {
 
-module.exports = {
-  index: function (req, res) {
-    res.send({});
+  //----------------------------------------------------------------------------
+
+  readNextLottery: function (req, res) {
+    var LotteryService = new sails.services.lottery();
+    LotteryService.getNextLottery(function (err, result) {
+      if (err) {
+        return res.serverError(err);
+      }
+      res.json(result);
+    });
+  },
+
+  //----------------------------------------------------------------------------
+
+  readGlobals: function (req, res) {
+    var PublicService = new sails.services.public();
+
+    PublicService.readGlobals(function (err, result) {
+      if (err) {
+        return res.serverError(err);
+      }
+      res.json({
+        serverTime: new Date().getTime(),
+        global: result
+      });
+    });
+  },
+
+  //----------------------------------------------------------------------------
+
+  readMobileSettings: function (req, res) {
+    var PublicService = new sails.services.public();
+
+    PublicService.readMobileSettings(function (err, result) {
+      if (err) {
+        return res.serverError(err);
+      }
+      res.json(result);
+    });
   }
 };
