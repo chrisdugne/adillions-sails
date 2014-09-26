2be91cb95a97bef8d4e728ab12c5d1c566df951a
chrisdugne <chris.dugne@uralys.com>
2014-09-26 23:13:49 +0200

    fixed update user thx Antoine :D

--- api/services/user.js
+++ api/services/user.js
@@ -4,31 +4,37 @@ var UserService = module.exports = function () {
     //--------------------------------------------------------------------------
 
     read: function (token, next) {
+      console.log(token);
       User
         .findOne()
         .where({
           auth_token: token
         })
-        .then(function (user) {
-          user.username = 'popi';
-          console.log(user);
-          user.save().then(function (user) {
-            sails.models.ticket
-              .find()
-              .limit(30)
-              .where({
-                player_uid: user.uid
-              })
-              .then(function (tickets) {
-                user.lotteryTickets = tickets;
-                next(null, user);
-              });
-          });
+        .then(function updateUser(user) {
+          console.log(user.uid);
+          user.username = 'popi333';
+          return user.save();
+        }).then(function findTicket(user) {
+          console.log(user.username);
+          return [user, sails.models.ticket
+            .find()
+            .limit(30)
+            .where({
+              player_uid: user.uid
+            })
+          ];
+        }).spread(function updateUserTickets(user, tickets) {
+          user.lotteryTickets = tickets;
+          return user;
+        })
+        .then(function done(user) {
+          next(null, user);
         })
         .fail(function (err) {
           sails.log.error('Lottery#getNextLottery Service: query fails', err);
           next(err);
         });
+
     },
 
     //--------------------------------------------------------------------------
